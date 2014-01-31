fca0e9327c76d6680122bf8bb180e38dc9080508
Robin Thrift <robin@we-two-design.com>
2014-01-31 16:39:50 +0100

    fisrt basic version

---
+++ index.js
@@ -0,0 +1,31 @@
+var _  = require('lodash'),
+    es = require('event-stream');
+
+var bricksNlogs = {
+    _s: es.pause(),
+
+    write: function(data) {
+        this._s.write(JSON.stringify(data) + "\n");
+    },
+
+    log: function(level, msgs) {
+        _.each(msgs, function(msg) {
+            this.write({
+                level: level,
+                msg: msg
+            });
+        }.bind(this));
+    },
+
+    info: function() {
+        this.log('info', _.toArray(arguments));
+    },
+
+    attach: function(listener) {
+        this._s.pipe(listener);
+    }
+};
+
+
+
+module.exports = bricksNlogs;

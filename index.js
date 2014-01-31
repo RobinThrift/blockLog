db58902a0c349b157a5688633dc305ebd87028ae
Robin Thrift <robin@we-two-design.com>
2014-01-31 18:56:53 +0100

    added formatted (plain) text streaming

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ var es = require('event-stream');
 var bricksNlogs = {
     _s: es.pause(),
 
+    jsonStream: es.stringify,
+     
+    txtStream: function () { 
+        return es.map(function(data, cb) { 
+            cb(null, '[' + data.level.toUpperCase() + '] ' + data.msg + '\n');
+        });
+    },
+
     write: function(data) {
-        this._s.write(JSON.stringify(data) + "\n");
+        this._s.write(data);
     },
 
     log: function(level, msgs) {
@@ -28,8 +36,22 @@ var bricksNlogs = {
         this.log('error', _.toArray(arguments));
     },
 
-    attach: function(listener) {
-        this._s.pipe(listener);
+    attach: function(listener, type) {
+        switch (type) {
+            case 'json':
+                this._s.pipe(this.jsonStream()).pipe(listener);
+            break;
+            case 'raw':
+                this._s.pipe(listener);
+            break;
+            case 'txt':
+            default:
+                this._s.pipe(this.txtStream()).pipe(listener);
+            break;
+        }
+    },
+
+
     }
 };
 

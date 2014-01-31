60b976c17b7956a6d6b5525e01dde5e7101127ca
Robin Thrift <robin@we-two-design.com>
2014-01-31 17:07:42 +0100

    added test for file writing

--- test/main.js
+++ test/main.js
@@ -1,5 +1,6 @@
 var should = require('should'),
-    bnl    = require('../');
+    bnl    = require('../'),
+    fs     = require('fs');
 
 require('mocha');
 
@@ -17,6 +18,30 @@ describe('blocksNlogs logging lib', function() {
 
             bnl.info('test', 'blub');
         });
+
+        it('should pipe to a file', function(done) {
+
+            fs.writeFileSync('test/fixtures/test.txt', '');
+
+            var ws = fs.createWriteStream('test/fixtures/test.txt', {encoding: 'utf8'});
+
+            bnl.attach(ws);
+
+            ws.once('finish', function(src) {
+                var fixt = fs.readFileSync('test/fixtures/fixt.txt', {encoding: 'utf8'}),
+                    res  = fs.readFileSync('test/fixtures/test.txt', {encoding: 'utf8'});
+
+                res.should.be.equal(fixt);
+                done();
+            });
+
+            bnl.info('test');
+
+            ws.end();
+
+        });
     });
+
+
 
 });

ade9faa730cf0c7803edeae75ee0147e703827dc
Robin Thrift <thrift.d.robin@googlemail.com>
2014-02-02 18:46:31 +0100

    renamed "log to file" test files

--- test/main.js
+++ test/main.js
@@ -22,16 +22,16 @@ describe('blockLog logging lib – ', function() {
 
         it('should pipe to a file', function(done) {
 
-            fs.writeFileSync('test/fixtures/test.txt', '');
+            fs.writeFileSync('test/fixtures/filetest.txt', '');
 
             var log = new blockLog('file-log-stream'),
-                ws = fs.createWriteStream('test/fixtures/test.txt', {encoding: 'utf8'});
+                ws = fs.createWriteStream('test/fixtures/filetest.txt', {encoding: 'utf8'});
 
             log.attach('testFile', ws, 'json');
 
             ws.once('finish', function() {
-                var fixt = fs.readFileSync('test/fixtures/fixt.txt', {encoding: 'utf8'}),
-                    res  = fs.readFileSync('test/fixtures/test.txt', {encoding: 'utf8'});
+                var fixt = fs.readFileSync('test/fixtures/filefixt.txt', {encoding: 'utf8'}),
+                    res  = fs.readFileSync('test/fixtures/filetest.txt', {encoding: 'utf8'});
 
                 res.should.be.equal(fixt);
                 done();
